0f67aad7389aed453257a0ba023b156bf36c30d3
Terje Tyldum <terjetyl@gmail.com>
2014-01-13 00:38:23 +0100

    sudoku almost done

--- src/Euler/Sudoku.fs
+++ src/Euler/Sudoku.fs
@@ -1,8 +1,5 @@
 ﻿namespace Terje
 
-open Microsoft.FSharp
-open MathNet.Numerics.LinearAlgebra.Complex
-
 module Sudoku =
     // Input puzzle
     let sudokuPuzzle = array2D [[0;0;0;0;6;0;4;0;0];
@@ -15,17 +12,9 @@ module Sudoku =
                                 [0;0;6;5;0;0;0;9;0];
                                 [0;0;4;0;1;0;0;0;0];]
 
-    let m = new DenseMatrix(3,3)
-
-    let hasNineNumbers (x:int[]) =
-        x 
-        |> Array.filter (fun x -> x > 0)
-        |> Array.length = 9
-
     let getUsedNumbers (x:int[]) =
         x 
         |> Array.filter (fun x -> x > 0)
-
 
     let flatten (matrix:'a[,]) = matrix |> Seq.cast<'a> |> Seq.toArray
 
@@ -36,13 +25,14 @@ module Sudoku =
         flatten matrix.[r..r,*]
 
     let getSquare s (vars: 'a[,]) = 
-        let x = s % 3
-        let y = s / 3
+        let x = s / 3
+        let y = s % 3
         let startx, endx = x * 3, (x + 1) * 3 - 1
         let starty, endy = y * 3, (y + 1) * 3 - 1
         flatten vars.[startx .. endx, starty .. endy]
 
     let getPossibles (x:int, y:int) = 
+
         let colNumbers = 
             (getCol x sudokuPuzzle)
             |> getUsedNumbers
@@ -54,23 +44,52 @@ module Sudoku =
         let getSquareNumber (x, y) =
             (x/3) + ((y/3) * 3)
 
-        let squareNumbers() = 
+        let squareNumbers = 
             getSquare (getSquareNumber(x,y)) sudokuPuzzle
+            |> getUsedNumbers
 
-        let findPossibles = 
-             System.Linq.Enumerable.Except([1..9], Array.append rowNumbers colNumbers)
-             |> Seq.toArray
+        let findPossibles() = 
+            let usedNumbers = Array.append (Array.append rowNumbers colNumbers) squareNumbers
+            System.Linq.Enumerable.Except([1..9], usedNumbers)
+            |> Seq.toArray
 
-        findPossibles
+        findPossibles()
 
-    
+    let augmentWithPossibles x y v = 
+        if v > 0 then
+            (v, Array.empty)
+        else (v, getPossibles(y, x))
+
     let checkBoard() = 
+        let tmp = 
+            sudokuPuzzle 
+            |> Array2D.mapi augmentWithPossibles
+
         for x in 0..8 do
             for y in 0..8 do
-                let possibles = (x, y, getPossibles(x, y))
-                if Seq.length possibles = 1 then
-                    printfn "%d,%d can only have %d" x y (Seq.head possibles)
-                else printfn "%d, %d, %A" x y possibles
+                if Array.length (snd tmp.[x,y]) = 1 then 
+                    printfn "%d, %d has a unique possibility %A" x y tmp
+
+        let findIndexOfArrayThatHasNum (x:int, arrayWithArrays) = 
+            arrayWithArrays
+            |> Seq.findIndex (fun x -> x |> Seq.exists (fun i -> i = x))
+
+        let findUniquePossibiltiesInCols() = 
+            for x in 0..8 do
+                let uniques = 
+                    tmp.[*,x] 
+                    |> Seq.map snd 
+                let uniqueNum = 
+                    uniques
+                    |> Seq.concat
+                    |> Seq.groupBy (fun x -> x) 
+                    |> Seq.filter (fun x -> Seq.length (snd x) = 1)
+                if Seq.length uniqueNum > 0 then
+                    printfn "Found unique num for pos %d,%A %A" x (findIndexOfArrayThatHasNum((Seq.head uniqueNum), uniques)) uniqueNum
+            printfn "Done"
+
+        findUniquePossibiltiesInCols()
+
         
         
 
