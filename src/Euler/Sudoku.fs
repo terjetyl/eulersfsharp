4025219e6fba35f5d5ccdb4a6f0b154cb9bcc7c6
Terje Tyldum <terjetyl@gmail.com>
2014-01-17 00:11:04 +0100

    dd

--- src/Euler/Sudoku.fs
+++ src/Euler/Sudoku.fs
@@ -2,144 +2,117 @@
 
 module Sudoku =
     // Input puzzle
-    let sudokuPuzzle = array2D [[0;0;0;0;6;0;4;0;0];
-                                [0;5;0;0;0;3;6;0;0];
-                                [1;0;0;0;0;5;0;0;0];
-                                [0;4;1;0;0;0;0;0;0];
-                                [0;9;0;0;0;0;0;2;0];
-                                [5;0;2;0;0;0;3;4;0];
-                                [3;0;0;7;0;0;0;0;0];
-                                [0;0;6;5;0;0;0;9;0];
-                                [0;0;4;0;1;0;0;0;0];]
+    let puzzle = array2D [[0;0;0;0;6;0;4;0;0];
+                          [0;5;0;0;0;3;6;0;0];
+                          [1;0;0;0;0;5;0;0;0];
+                          [0;4;1;0;0;0;0;0;0];
+                          [0;9;0;0;0;0;0;2;0];
+                          [5;0;2;0;0;0;3;4;0];
+                          [3;0;0;7;0;0;0;0;0];
+                          [0;0;6;5;0;0;0;9;0];
+                          [0;0;4;0;1;0;0;0;0];]
 
-    let getPossibles (x:int, y:int) = 
+    let getCol index (matrix:_[,]) =
+        matrix.[*,index]
 
-        let getUsedNumbers (x:int[]) =
+    let getRow index (matrix:_[,]) =
+        matrix.[index,*]
+
+    let getSquare index (vars: 'a[,]) = 
+        let x = index / 3
+        let y = index % 3
+        let startx, endx = x * 3, (x + 1) * 3 - 1
+        let starty, endy = y * 3, (y + 1) * 3 - 1
+        let flatten (matrix:'a[,]) = matrix |> Seq.cast<'a> |> Seq.toArray
+        flatten vars.[startx .. endx, starty .. endy]
+
+    let findPossibleNumbers (x:int, y:int, puzzle:_[,]) = 
+        let findUsedNumbers (x:int[]) =
             x 
             |> Array.filter (fun x -> x > 0)
-
-        let flatten (matrix:'a[,]) = matrix |> Seq.cast<'a> |> Seq.toArray
-
-        let getCol c (matrix:_[,]) =
-            flatten matrix.[*,c..c] 
- 
-        let getRow r (matrix:_[,]) =
-            flatten matrix.[r..r,*]
-
-        let getSquare s (vars: 'a[,]) = 
-            let x = s / 3
-            let y = s % 3
-            let startx, endx = x * 3, (x + 1) * 3 - 1
-            let starty, endy = y * 3, (y + 1) * 3 - 1
-            flatten vars.[startx .. endx, starty .. endy]
-
         let colNumbers = 
-            (getCol x sudokuPuzzle)
-            |> getUsedNumbers
-
+            (getCol x puzzle)
+            |> findUsedNumbers
         let rowNumbers = 
-            (getRow y sudokuPuzzle)
-            |> getUsedNumbers
-
-        let getSquareNumber (x, y) =
-            (x/3) + ((y/3) * 3)
-
+            (getRow y puzzle)
+            |> findUsedNumbers
         let squareNumbers = 
-            getSquare (getSquareNumber(x,y)) sudokuPuzzle
-            |> getUsedNumbers
-
+            let getSquareNumber (x, y) =
+                (x/3) + ((y/3) * 3)
+            getSquare (getSquareNumber(x,y)) puzzle
+            |> findUsedNumbers
         let possibles() = 
             let usedNumbers = Array.append (Array.append rowNumbers colNumbers) squareNumbers
-            System.Linq.Enumerable.Except([1..9], usedNumbers)
-            |> Seq.toArray
-
+            set [1..9] - set usedNumbers
         possibles()
 
-    let findPossibles x y v = 
-        if v > 0 then
-            (v, Array.empty)
-        else (v, getPossibles(y, x))
+    type Pos = { X:int; Y:int }
+    type SudokuVal = { Pos: Pos; Value: int }
 
-    let checkBoard() = 
-        let tmp = 
-            sudokuPuzzle 
+    let solve (puzzle:_[,]) = 
+        let findPossibles x y v = 
+            if v > 0 then // already has value
+                Set.empty
+            else findPossibleNumbers(y, x, puzzle)
+        let matrixOfPossibilities = 
+            puzzle 
             |> Array2D.mapi findPossibles
 
-        let existsInArray x arr = 
-            Seq.exists (fun c -> c = x) arr
+        let findNumbersThatAppearsOnlyOnceInArrayOfPossibilities arrayOfArrays =
+            let uniqueNumbers =
+                arrayOfArrays 
+                |> Seq.concat // concat all numbers in array
+                |> Seq.groupBy (fun x -> x) // groupBy to find number of occurrence
+                |> Seq.filter (fun x -> Seq.length (snd x) = 1) // filter out only numbers appearing once meaning its the only place this number could be
+                |> Seq.map fst // we only need the key of the groupby result
+            // return a touple of uniqueNumbers and their index in the arrayOfArrays
+            let findIndexOfArray x = 
+                arrayOfArrays |> Seq.findIndex (fun array -> Seq.exists (fun y -> y = x) array)
+            uniqueNumbers |> Seq.map (fun x -> ((findIndexOfArray x), x))
 
-        let findIndexOfArrayThatHasNum (x:int, arrayWithArrays) = 
-            arrayWithArrays
-            |> Seq.findIndex (fun y -> existsInArray x y)
+        let checkColsForUniquePossibilities() = 
+            [0..8] 
+            |> Seq.map (fun x -> findNumbersThatAppearsOnlyOnceInArrayOfPossibilities (getCol x matrixOfPossibilities))
+            |> Seq.mapi (fun i x -> (i, x)) // we need to know the index
+            |> Seq.filter (fun x -> Seq.length (snd x) > 0) // filter out empty lists
+            |> Seq.map (fun x -> (fst x, Seq.head (snd x))) // we dont need to keep the record in a seq
+            |> Seq.map (fun x -> { Pos = { X = fst x; Y = fst (snd x) }; Value = snd (snd x) }) // finally set up the records as needed
 
-        let findUniquePossibiltiesInCols() = 
-            for x in 0..8 do
-                let uniques = 
-                    tmp.[*,x] 
-                    |> Seq.map snd 
-                let uniqueNum = 
-                    uniques
-                    |> Seq.concat
-                    |> Seq.groupBy (fun x -> x) 
-                    |> Seq.filter (fun x -> Seq.length (snd x) = 1)
-                    |> Seq.map fst
-                if Seq.length uniqueNum > 0 then
-                    let num = Seq.head uniqueNum
-                    let y = findIndexOfArrayThatHasNum(num, uniques)
-                    printfn "Found unique num for pos %d,%d %d" x y num
-                    sudokuPuzzle.[y,x] <- num
-            printfn "%A" sudokuPuzzle
+        let checkRowsForUniquePossibilities() = 
+            [0..8] 
+            |> Seq.map (fun x -> findNumbersThatAppearsOnlyOnceInArrayOfPossibilities (getRow x matrixOfPossibilities))
+            |> Seq.mapi (fun i x -> (i, x)) // we need to know the index
+            |> Seq.filter (fun x -> Seq.length (snd x) > 0) // filter out empty lists
+            |> Seq.map (fun x -> (fst x, Seq.head (snd x))) // we dont need to keep the record in a seq
+            |> Seq.map (fun x -> { Pos = { Y = fst x; X = fst (snd x) }; Value = snd (snd x) }) // finally set up the records as needed
 
-        let findUniquePossibiltiesInRows() = 
-            for x in 0..8 do
-                let uniques = 
-                    tmp.[x,*] 
-                    |> Seq.map snd 
-                let uniqueNum = 
-                    uniques
-                    |> Seq.concat
-                    |> Seq.groupBy (fun x -> x) 
-                    |> Seq.filter (fun x -> Seq.length (snd x) = 1)
-                    |> Seq.map fst
-                if Seq.length uniqueNum > 0 then
-                    let num = Seq.head uniqueNum
-                    let y = findIndexOfArrayThatHasNum(num, uniques)
-                    printfn "Found unique num for pos %d,%d %d" y x num
-                    sudokuPuzzle.[x,y] <- num
-            printfn "%A" sudokuPuzzle
+        let checkSquaresForUniquePossibilities() = 
+            let getCoords (squareIndex, arrayIndex) = 
+                let x = (squareIndex % 3 * 3) + (arrayIndex % 3)
+                let y = (squareIndex / 3 * 3) + (arrayIndex / 3)
+                (x, y)
+            let getFinalRecord x = 
+                let squareIndex = fst x
+                let arrayIndex = fst (snd x)
+                let value = snd (snd x)
+                let coords = getCoords (squareIndex, arrayIndex)
+                let record = { Pos = { Y = snd coords; X = fst coords }; Value = value }
+                record
+            [0..8] 
+            |> Seq.map (fun x -> findNumbersThatAppearsOnlyOnceInArrayOfPossibilities (getSquare x matrixOfPossibilities))
+            |> Seq.mapi (fun i x -> (i, x)) // we need to know the index
+            |> Seq.filter (fun x -> Seq.length (snd x) > 0) // filter out empty lists
+            |> Seq.map (fun x -> (fst x, Seq.head (snd x))) // we dont need to keep the record in a seq
+            |> Seq.map getFinalRecord // finally set up the records as needed
 
-        let flatten (matrix:'a[,]) = matrix |> Seq.cast<'a> |> Seq.toArray
+        let findValues () = 
+            checkColsForUniquePossibilities()
+            |> Seq.append (checkRowsForUniquePossibilities())
+            |> Seq.append (checkSquaresForUniquePossibilities())
 
-        let getSquare s (vars: 'a[,]) = 
-            let x = s / 3
-            let y = s % 3
-            let startx, endx = x * 3, (x + 1) * 3 - 1
-            let starty, endy = y * 3, (y + 1) * 3 - 1
-            flatten vars.[startx .. endx, starty .. endy]
+        let foundValues = findValues()
+        foundValues |> List.iter
 
-        let findUniquePossibiltiesInSquares() = 
-            for x in 0..8 do
-                let uniques = 
-                    getSquare x tmp 
-                    |> Seq.map snd 
-                let uniqueNum = 
-                    uniques
-                    |> Seq.concat
-                    |> Seq.groupBy (fun x -> x) 
-                    |> Seq.filter (fun x -> Seq.length (snd x) = 1)
-                    |> Seq.map fst
-                if Seq.length uniqueNum > 0 then
-                    let num = Seq.head uniqueNum
-                    let y = findIndexOfArrayThatHasNum(num, uniques)
-                    printfn "Found unique num for pos %d,%d %d" y x num
-                    sudokuPuzzle.[x,y] <- num
-            printfn "%A" sudokuPuzzle
-
-        findUniquePossibiltiesInCols()
-        findUniquePossibiltiesInRows()
-        findUniquePossibiltiesInSquares()
-
-        
         
 
     
